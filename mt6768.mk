64d77c3d93c8c7105096dc9b0a69db6d3e06e999
bengris32 <bengris32@protonmail.ch>
2022-10-04 20:50:04 +0100

    mt6768-common: Build stub mtkpower service * It seems even after adding a dummy libmtkperf_client library,   camera still refuses to work, it seems MediaTek have used a   mix of that client library, and direct HIDL calls to the power   implementation. Build a stub mtkpower service to get around   this limitation.
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- mt6768.mk
+++ mt6768.mk
@@ -266,7 +266,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.mediatek.hardware.mtkpower@1.0.vendor \
     vendor.mediatek.hardware.mtkpower@1.1.vendor \
-    vendor.mediatek.hardware.mtkpower@1.2.vendor
+    vendor.mediatek.hardware.mtkpower@1.2.vendor \
+    vendor.mediatek.hardware.mtkpower@1.2-service.stub
 
 # PowerOffAlarm
 PRODUCT_PACKAGES += \
